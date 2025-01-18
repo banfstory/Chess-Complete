a5e350dd2c62d6604e5cb67237396fbf176c240b
denis n <denis_chi_yiu@hotmail.com>
2025-01-18 12:42:29 +1100

    Changed point capture system

--- ChessGame/Chess/AIMove/SquarePoints.cs
+++ ChessGame/Chess/AIMove/SquarePoints.cs
@@ -5,26 +5,26 @@ namespace Chess.AIMove
     static class SquarePoints
     {
         // this determine the points to increase the captured square by depending on the target
-        static public int move = 3;
+        static public int move = 1;
 
         static public Dictionary<ChessGame.pieceName, int> attack = new Dictionary<ChessGame.pieceName, int>()
         {
-            { ChessGame.pieceName.King, 5 },
-            { ChessGame.pieceName.Queen, 5 },
-            { ChessGame.pieceName.Rook, 5 },
-            { ChessGame.pieceName.Bishop, 5 },
-            { ChessGame.pieceName.Knight, 4 },
-            { ChessGame.pieceName.Pawn, 4 }      
+            { ChessGame.pieceName.King, 4 },
+            { ChessGame.pieceName.Queen, 4 },
+            { ChessGame.pieceName.Rook, 3 },
+            { ChessGame.pieceName.Bishop, 3 },
+            { ChessGame.pieceName.Knight, 3 },
+            { ChessGame.pieceName.Pawn, 2 }      
         };
 
         static public Dictionary<ChessGame.pieceName, int> defense = new Dictionary<ChessGame.pieceName, int>()
         {
             { ChessGame.pieceName.King, 0 }, // you cannot trade a king therefore it should be worth nothing defending that square
-            { ChessGame.pieceName.Queen, 5 },
-            { ChessGame.pieceName.Rook, 5 },
-            { ChessGame.pieceName.Bishop, 5 },
-            { ChessGame.pieceName.Knight, 5 },
-            { ChessGame.pieceName.Pawn, 4 }
+            { ChessGame.pieceName.Queen, 4 },
+            { ChessGame.pieceName.Rook, 3 },
+            { ChessGame.pieceName.Bishop, 3 },
+            { ChessGame.pieceName.Knight, 3 },
+            { ChessGame.pieceName.Pawn, 2 }
         };
     }
 }
